680c4724f2399c37de821dfdc54484f31371f6d4
SeongJin Oh <guk0230@naver.com>
2023-01-08 16:57:34 +0900

    알고리즘 수업 - 너비 우선 탐색2_24445.py

---
+++ 그래프/알고리즘 수업 - 너비 우선 탐색2_24445.py
@@ -0,0 +1,32 @@
+# 알고리즘 수업 - 너비 우선 탐색2_24445_BFS
+
+from collections import deque
+import sys
+input=sys.stdin.readline
+
+N,M,R=map(int,input().split())
+graph=[[] for _ in range(N+1)]
+visited=[0]*(N+1)
+
+for _ in range(M):
+  x,y=map(int,input().split())
+  graph[x].append(y)
+  graph[y].append(x)
+
+for i in range(1,N+1):
+  graph[i].sort(reverse=True)
+
+def bfs(v):
+  cnt=1
+  q=deque([v])
+  visited[v]=1
+  while q:
+    v=q.popleft()
+    for i in graph[v]:
+      if visited[i]==0:
+        cnt+=1
+        visited[i]=cnt
+        q.append(i)
+
+bfs(R)
+print(*[visited[i] for i in range(1,N+1)],sep='\n')
