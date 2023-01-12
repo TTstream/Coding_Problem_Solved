e7e5dfdd974a994b90627a9874f9e3dba030b38f
SeongJin Oh <guk0230@naver.com>
2023-01-12 09:10:06 +0900

    알고리즘 수업 - 깊이 우선 탐색 2_24480.py

---
+++ 그래프/알고리즘 수업 - 깊이 우선 탐색 2_24480.py
@@ -0,0 +1,29 @@
+# 알고리즘 수업 - 깊이 우선 탐색 2_24480_DFS
+
+import sys
+input=sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+N,M,R=map(int,input().split())
+graph=[[] for _ in range(N+1)]
+visited=[0]*(N+1)
+
+for _ in range(M):
+  x,y=map(int,input().split())
+  graph[x].append(y)
+  graph[y].append(x)
+
+for i in range(1,N+1):
+  graph[i].sort(reverse=True)
+
+def dfs(v):
+  global cnt
+  visited[v]=cnt
+  for i in graph[v]:
+    if visited[i]==0:
+      cnt+=1
+      dfs(i)
+      
+cnt=1
+dfs(R)
+print(*[visited[i] for i in range(1,N+1)],sep='\n')
